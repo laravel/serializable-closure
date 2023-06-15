1241bd8859c7dca9aeb14eb2177d92ce05a43332
Flavio Heleno <flaviohbatista@gmail.com>
2023-06-15 10:56:15 -0300

    Fix code styling

--- tests/Fixtures/Sample.php
+++ tests/Fixtures/Sample.php
@@ -2,7 +2,11 @@
 
 namespace Tests\Fixtures;
 
-class Sample {
-  public const C = 'CONST';
-  public function __construct($a1 = null, $a2 = null) {}
+class Sample
+{
+    public const C = 'CONST';
+
+    public function __construct($a1 = null, $a2 = null)
+    {
+    }
 }
