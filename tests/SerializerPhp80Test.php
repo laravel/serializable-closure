61ee141f7f99686cc4f3f189b03b92204d9f5b16
Nuno Maduro <enunomaduro@gmail.com>
2023-11-07 17:10:22 +0000

    Adds PHP 8.0 tests

--- tests/SerializerPhp80Test.php
+++ tests/SerializerPhp80Test.php
@@ -77,6 +77,78 @@ class SerializerPhp80NamedArguments
     }
 }
 
+function serializer_php_80_switch_statement_test_is_two($a)
+{
+    return $a === 2;
+}
+
+class SerializerPhp80SwitchStatementClass
+{
+    public static function isThree($a)
+    {
+        return $a === 3;
+    }
+
+    public function isFour($a)
+    {
+        return $a === 4;
+    }
+}
+
+class SerializerPhp80Class {};
+
+test('instanceof', function () {
+    $closure = function (object $a): array {
+        $b = $a instanceof DateTime || $a instanceof SerializerPhp80Class;
+
+        return [
+            $b,
+            ($a instanceof DateTime || $a instanceof SerializerPhp80Class),
+            (function (object $a): bool {
+                return ($a instanceof DateTime || $a instanceof SerializerPhp80Class) === true;
+            })(a: $a),
+        ];
+
+    };
+
+    $u = s($closure);
+
+    expect($u(new DateTime))->toEqual([true, true, true])
+        ->and($u(new SerializerPhp80Class))->toEqual([true, true, true])
+        ->and($u(new stdClass))->toEqual([false, false, false]);
+})->with('serializers');
+
+test('switch statement', function () {
+    $closure = function ($a) {
+        switch (true) {
+            case $a === 1:
+                return 'one';
+            case serializer_php_74_switch_statement_test_is_two(a: $a):
+                return 'two';
+            case SerializerPhp80SwitchStatementClass::isThree(a: $a):
+                return 'three';
+            case (new SerializerPhp80SwitchStatementClass)->isFour(a: $a):
+                return 'four';
+            case ($a instanceof SerializerPhp80SwitchStatementClass):
+                return 'five';
+            case ($a instanceof DateTime):
+                return 'six';
+            default:
+                return 'other';
+        }
+    };
+
+    $u = s($closure);
+
+    expect($u(1))->toEqual('one')
+        ->and($u(2))->toEqual('two')
+        ->and($u(3))->toEqual('three')
+        ->and($u(4))->toEqual('four')
+        ->and($u(new SerializerPhp80SwitchStatementClass))->toEqual('five')
+        ->and($u(new DateTime))->toEqual('six')
+        ->and($u(999))->toEqual('other');
+})->with('serializers');
+
 function match_statement_test_is_two($a)
 {
     return $a === 2;
@@ -101,7 +173,7 @@ test('match statement', function () {
             $a === 1 => 'one',
             match_statement_test_is_two($a) => 'two',
             MatchStatementClass::isThree($a) => 'three',
-            (new MatchStatementClass)->isFour($a) => 'four',
+            (new MatchStatementClass)->isFour(a: $a) => 'four',
             $a instanceof MatchStatementClass => 'five',
             $a instanceof DateTime => 'six',
             default => 'other',
