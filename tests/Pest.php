c2455ddba4a1371bdaaacbb0e08c0d901dec46c1
Mior Muhammad Zaki <crynobone@gmail.com>
2024-10-01 17:57:46 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Pest.php
+++ tests/Pest.php
@@ -58,6 +58,7 @@ expect()->extend('toBeCode', function ($expected) {
  * Returns the given closure after serialize/unserialize.
  *
  * @param  \Closure  $closure
+ * @param  string|null  $serializer
  * @return \Closure
  */
 function s($closure)
