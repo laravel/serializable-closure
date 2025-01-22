b9082aa0341edffb16e2eea8ef3e4fbf1c903287
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2025-01-22 20:23:59 +0100

    WIP

--- tests/ReflectionClosure6Test.php
+++ tests/ReflectionClosure6Test.php
@@ -8,8 +8,12 @@ use Tests\Fixtures\Model;
 use Tests\Fixtures\RegularClass;
 
 test('resolve instanceof with class_exists()', function () {
-	$f1 = fn (Baz $a) => $this instanceof (class_exists(Foo::class) ? Foo::class : Forest::class);
-	$e1 = 'fn (\Foo\Bar $a) => $this instanceof (class_exists(\Foo:class) ? \Foo::class : \Foo\Baz\Qux\Forest::class)';
+	$f1 = fn (Baz $a) => $this instanceof (Forest::class); // Turning `instanceof` into an expression results into this incorrect compilation
+	$e1 = 'fn (\Foo\Bar $a) => $this instanceof (\Foo\Baz\Qux\Forest::class)';
+	
+	$f2 = fn (Baz $a) => $this instanceof (class_exists(Foo::class) ? Foo::class : Forest::class);
+	$e2 = 'fn (\Foo\Bar $a) => $this instanceof (class_exists(\Foo:class) ? \Foo::class : \Foo\Baz\Qux\Forest::class)';
 	
     expect($f1)->toBeCode($e1);
+    expect($f2)->toBeCode($e2);
 });
