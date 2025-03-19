b352cf0534aa1ae6b4d825d1e762e35d43f8a841
Jean-Luc <jlh@gmx.ch>
2025-03-19 14:51:03 +0100

    Fix namespaces for PHP 8.4 (#112)
    * Parse namespaces from tokens, don't relying on getNamespaceName()
    
    ReflectionFunction::getNamespaceName() no longer returns a meaningful
    namespace for closures starting with PHP 8.4.
    
    * Add tests for namespace parsing
    
    Especially for resolving relative namespaces in PHP 8.4.
    
    * Fix wrongly considering 'use' statements from other namespaces
    
    The only case not handled is to ignore 'use' statements that appear
    after usage within the same namespace.
    
    * .styleci.yml: Exclude tests
    
    The unused 'use' statements are essential for the tests.

--- src/Support/ReflectionClosure.php
+++ src/Support/ReflectionClosure.php
@@ -865,13 +865,18 @@ class ReflectionClosure extends ReflectionFunction
      */
     protected function getClasses()
     {
-        $key = $this->getHashedFileName();
+        $line = $this->getStartLine();
 
-        if (! isset(static::$classes[$key])) {
-            $this->fetchItems();
+        foreach ($this->getStructures() as $struct) {
+            if ($struct['type'] === 'namespace' &&
+                $struct['start'] <= $line &&
+                $struct['end'] >= $line
+            ) {
+                return $struct['classes'];
+            }
         }
 
-        return static::$classes[$key];
+        return [];
     }
 
     /**
@@ -945,14 +950,36 @@ class ReflectionClosure extends ReflectionFunction
         $alias = '';
         $isFunc = $isConst = false;
 
-        $startLine = $endLine = 0;
+        $startLine = $lastKnownLine = 0;
         $structType = $structName = '';
         $structIgnore = false;
 
+        $namespace = '';
+        $namespaceStartLine = 0;
+        $namespaceBraced = false;
+        $namespaceClasses = [];
+
         foreach ($tokens as $token) {
+            if (is_array($token)) {
+                $lastKnownLine = $token[2];
+            }
+
             switch ($state) {
                 case 'start':
                     switch ($token[0]) {
+                        case T_NAMESPACE:
+                            $structures[] = [
+                                'type' => 'namespace',
+                                'name' => $namespace,
+                                'start' => $namespaceStartLine,
+                                'end' => $token[2] - 1,
+                                'classes' => $namespaceClasses,
+                            ];
+                            $namespace = '';
+                            $namespaceClasses = [];
+                            $state = 'namespace';
+                            $namespaceStartLine = $token[2];
+                            break;
                         case T_CLASS:
                         case T_INTERFACE:
                         case T_TRAIT:
@@ -977,6 +1004,33 @@ class ReflectionClosure extends ReflectionFunction
                         case T_OBJECT_OPERATOR:
                         case T_DOUBLE_COLON:
                             $state = 'invoke';
+                            break;
+                        case '}':
+                            if ($namespaceBraced) {
+                                $structures[] = [
+                                    'type' => 'namespace',
+                                    'name' => $namespace,
+                                    'start' => $namespaceStartLine,
+                                    'end' => $lastKnownLine,
+                                    'classes' => $namespaceClasses,
+                                ];
+                                $namespaceBraced = false;
+                                $namespace = '';
+                                $namespaceClasses = [];
+                            }
+                            break;
+                    }
+                    break;
+                case 'namespace':
+                    switch ($token[0]) {
+                        case T_STRING:
+                        case T_NAME_QUALIFIED:
+                            $namespace = $token[1];
+                            break;
+                        case ';':
+                        case '{':
+                            $state = 'start';
+                            $namespaceBraced = $token[0] === '{';
                             break;
                     }
                     break;
@@ -1022,6 +1076,7 @@ class ReflectionClosure extends ReflectionFunction
                                     $constants[$alias] = $name;
                                 } else {
                                     $classes[strtolower($alias)] = $name;
+                                    $namespaceClasses[strtolower($alias)] = $name;
                                 }
                             }
                             $name = $alias = '';
@@ -1061,6 +1116,7 @@ class ReflectionClosure extends ReflectionFunction
                                     $constants[$alias] = $prefix.$name;
                                 } else {
                                     $classes[strtolower($alias)] = $prefix.$name;
+                                    $namespaceClasses[strtolower($alias)] = $prefix.$name;
                                 }
                             }
                             $name = $alias = '';
@@ -1118,21 +1174,25 @@ class ReflectionClosure extends ReflectionFunction
                                         'type' => $structType,
                                         'name' => $structName,
                                         'start' => $startLine,
-                                        'end' => $endLine,
+                                        'end' => $lastKnownLine,
                                     ];
                                 }
                                 $structIgnore = false;
                                 $state = 'start';
                             }
                             break;
-                        default:
-                            if (is_array($token)) {
-                                $endLine = $token[2];
-                            }
                     }
                     break;
             }
         }
+
+        $structures[] = [
+            'type' => 'namespace',
+            'name' => $namespace,
+            'start' => $namespaceStartLine,
+            'end' => PHP_INT_MAX,
+            'classes' => $namespaceClasses,
+        ];
 
         static::$classes[$key] = $classes;
         static::$functions[$key] = $functions;
@@ -1147,20 +1207,19 @@ class ReflectionClosure extends ReflectionFunction
      */
     protected function getClosureNamespaceName()
     {
-        $ns = $this->getNamespaceName();
+        $startLine = $this->getStartLine();
+        $endLine = $this->getEndLine();
 
-        $name = $this->getName();
-
-        // First class callables...
-        if ($name !== '{closure}'
-            && ! str_contains($name, '{closure:/')
-            && ! str_contains($name, '{closure:\\')
-            && empty($ns)
-            && ! is_null($this->getClosureScopeClass())) {
-            $ns = $this->getClosureScopeClass()->getNamespaceName();
+        foreach ($this->getStructures() as $struct) {
+            if ($struct['type'] === 'namespace' &&
+                $struct['start'] <= $startLine &&
+                $struct['end'] >= $endLine
+            ) {
+                return $struct['name'];
+            }
         }
 
-        return $ns;
+        return '';
     }
 
     /**
---
+++ tests/ReflectionClosureBracedNamespaceTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Space {
+    test('relative namespace (braced)', function () {
+        $f1 = fn (Foo $foo): Foo => new Foo();
+        $e1 = 'fn (\Space\Foo $foo): \Space\Foo => new \Space\Foo()';
+
+        $f2 = fn (Foo\Bar $fooBar): Foo\Bar => new Foo\Bar();
+        $e2 = 'fn (\Space\Foo\Bar $fooBar): \Space\Foo\Bar => new \Space\Foo\Bar()';
+
+        expect($f1)->toBeCode($e1);
+        expect($f2)->toBeCode($e2);
+    });
+}
+
+namespace Irrelevant {}
+
+namespace Sub\Space {
+    test('relative other namespace (braced)', function () {
+        $f1 = fn (Foo $foo): Foo => new Foo();
+        $e1 = 'fn (\Sub\Space\Foo $foo): \Sub\Space\Foo => new \Sub\Space\Foo()';
+
+        expect($f1)->toBeCode($e1);
+    });
+}
+
+namespace {
+    test('back to global namespace (braced)', function () {
+        $f1 = fn (Foo $foo): Foo => new Foo();
+        $e1 = 'fn (\Foo $foo): \Foo => new \Foo()';
+        expect($f1)->toBeCode($e1);
+    });
+}
+
+namespace Irrelevant {
+    // Shouldn't be used below, as not in the same namespace
+    use Wrong as Qux;
+}
+
+namespace Space {
+    test('no using use from other namespace', function () {
+        $f1 = fn (Qux $qux) => true;
+        $e1 = 'fn (\Space\Qux $qux) => true';
+
+        expect($f1)->toBeCode($e1);
+    });
+
+    // Shouldn't be used above, as declared after usage.  Not currently supported though.
+    // use AlsoWrong as Qux;
+}
