e327dd63271389c42862b287c84be38c6dfe6aa4
Jivan Flores <jivan.flores.108@gmail.com>
2026-01-15 17:41:45 -0800

    Fix virtual property serialization

--- tests/Php84Test.php
+++ tests/Php84Test.php
@@ -1,19 +1,52 @@
 <?php
 
-it('can serialize class with virtual property', function () {
-    $c = new VirtualPropWithPhp84();
+it('can serialize class with virtual properties', function () {
+    $binding = new ClosureBinding();
 
-    $f = function () use ($c) {
-        return $c;
-    };
+    $f = $binding->closure;
 
     $s1 = s($f);
 
-    expect('test')->toBe($s1()->test);
+    $result = $s1();
+
+    expect($result->virtualString)->toBe('virtual string')
+        ->and($result->virtualArray)->toBe(['virtual array'])
+        ->and($result->virtualObject)->toBeInstanceOf(VirtualObjectWithPhp84::class);
 })->with('serializers');
 
-class VirtualPropWithPhp84 {
-    public string $test {
-        get => 'test';
+/**
+ * Has a bound closure.
+ *
+ * This bound closure is needed to test `wrapClosures()` of `Native` and ensure it handles
+ * virtual properties correctly.
+ */
+class ClosureBinding {
+    public Closure $closure {
+        get {
+            $virtualProps = new VirtualPropWithPhp84();
+
+            return function () use ($virtualProps) {
+                // This binds the closure to `$this`
+                $this;
+
+                return $virtualProps;
+            };
+        }
     }
 }
+
+class VirtualPropWithPhp84 {
+    public string $virtualString {
+        get => 'virtual string';
+    }
+
+    public array $virtualArray {
+        get => ['virtual array'];
+    }
+
+    public VirtualObjectWithPhp84 $virtualObject {
+        get => new VirtualObjectWithPhp84();
+    }
+}
+
+class VirtualObjectWithPhp84 {}
